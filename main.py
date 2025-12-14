9532b8b3aa46a7c65951964229e1aaa94c104b32
LeoPro777 <leodfernandez2004@gmail.com>
2025-12-13 21:10:22 -0400

    Para que sea compatible con el servidor

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 if __name__ == "__main__":
     import uvicorn
+    import os
+
+    # Usar el puerto que asigne la nube (Render) o 10000 por defecto
+    port = int(os.environ.get("PORT", 10000))
 
     uvicorn.run("app.main:app", 
-                host="localhost", 
-                port=8000, 
-                #log_level="info",
-                reload=True)
-
-# Activar entorno .\venv\Scripts\activate
+                host="0.0.0.0",     # IMPORTANTE: 0.0.0.0 permite acceso externo
+                port=port, 
+                reload=False)       # False para producción (es más rápido)
